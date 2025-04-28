b2c29236bb834f6e762e93263933b4443565143a
Your Name <you@example.com>
2025-04-28 19:15:23 +0300

    fixes

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Contact
 
 
 @admin.register(Category)
@@ -11,3 +11,8 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price', 'category')
     list_filter = ('category',)
     search_fields = ('name', 'description')
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'phone')  # Поля для отображения в списке
+    search_fields = ('name', 'phone')       # Поля для поиска
