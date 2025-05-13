47e9c287194a4048b73b3f94d44687022fb772d1
Your Name <you@example.com>
2025-05-13 13:57:00 +0300

    add url

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),
 ]
