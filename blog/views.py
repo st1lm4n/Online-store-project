e1cdffe43eb429135ad64d2fc95f228e43d992c7
Your Name <you@example.com>
2025-05-15 15:34:01 +0300

    Creating a blog application

---
+++ blog/views.py
@@ -0,0 +1,42 @@
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from .models import Post
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        obj.views_count += 1
+        obj.save()
+        return obj
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content', 'preview', 'is_published']
+    template_name = 'blog/post_form.html'
+    success_url = reverse_lazy('post_list')
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'content', 'preview', 'is_published']
+    template_name = 'blog/post_form.html'
+
+    def get_success_url(self):
+        return reverse_lazy('post_detail', kwargs={'pk': self.object.pk})
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post_list')
