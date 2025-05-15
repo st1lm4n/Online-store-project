e96345bfec529027700d66ca76ceaaf1b34ed729
Your Name <you@example.com>
2025-05-15 15:31:38 +0300

    Switching controllers to CBV

--- catalog/views.py
+++ catalog/views.py
@@ -1,24 +1,28 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Contact, Product
 from django.shortcuts import redirect
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, TemplateView
+
+from blog.models import Post
 from .forms import ProductForm
-from django.core.paginator import Paginator
+from .models import Product
 
 
-def home(request):
-    products = Product.objects.all().order_by('-created_at')
-    return render(request, 'catalog/home.html', {'products': products})
+class HomeView(ListView):
+    model = Product
+    template_name = 'catalog/home.html'
+    context_object_name = 'products'
+    ordering = ['-created_at']
 
 
-def contacts(request):
-    if request.method == 'POST':
-        Contact.objects.create(
-            name=request.POST.get('name'),
-            phone=request.POST.get('phone'),
-            message=request.POST.get('message')
-        )
-        return render(request, 'catalog/contacts.html', {'success': True})
-    return render(request, 'catalog/contacts.html')
+class ContactsView(TemplateView):
+    template_name = 'catalog/contacts.html'
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+
+    def get_queryset(self):
+        return Post.objects.filter(is_published=True)
 
 
 def product_detail(request, pk):
@@ -35,11 +39,3 @@ def add_product(request):
     else:
         form = ProductForm()
     return render(request, 'catalog/add_product.html', {'form': form})
-
-
-def home(request):
-    product_list = Product.objects.all().order_by('-created_at')
-    paginator = Paginator(product_list, 6)  # 6 товаров на странице
-    page_number = request.GET.get('page')
-    products = paginator.get_page(page_number)
-    return render(request, 'catalog/home.html', {'products': products})
