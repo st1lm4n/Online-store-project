1b363459b2e5a0851af5c7b6aa90d8b412656f23
Your Name <you@example.com>
2025-05-27 13:31:12 +0300

    Configuring URL Routes

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,23 @@
 from django.urls import path
 
-from .views import HomeView, ContactsView, ProductDetailView
+from .views import (
+    ContactsView,
+    HomeView,
+    ProductCreateView,
+    ProductDeleteView,
+    ProductDetailView,
+    ProductUpdateView,
+)
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('contacts/', ContactsView.as_view(), name='contacts'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path("", HomeView.as_view(), name="home"),
+    path("contacts/", ContactsView.as_view(), name="contacts"),
+    path("product/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
+    path("product/create/", ProductCreateView.as_view(), name="product_create"),
+    path(
+        "product/<int:pk>/update/", ProductUpdateView.as_view(), name="product_update"
+    ),
+    path(
+        "product/<int:pk>/delete/", ProductDeleteView.as_view(), name="product_delete"
+    ),
 ]
