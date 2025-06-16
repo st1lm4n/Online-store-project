15b969b299443b55d0ec1d342986de8e04cbc837
Your Name <you@example.com>
2025-06-16 15:35:53 +0300

    URL Settings

--- blog/urls.py
+++ blog/urls.py
@@ -1,17 +1,10 @@
 from django.urls import path
-
-from .views import (
-    PostCreateView,
-    PostDeleteView,
-    PostDetailView,
-    PostListView,
-    PostUpdateView,
-)
+from . import views
 
 urlpatterns = [
-    path("", PostListView.as_view(), name="post_list"),
-    path("create/", PostCreateView.as_view(), name="post_create"),
-    path("<int:pk>/", PostDetailView.as_view(), name="post_detail"),
-    path("<int:pk>/update/", PostUpdateView.as_view(), name="post_update"),
-    path("<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/create/', views.PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
 ]
