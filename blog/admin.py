1a992839bb8a3bf155e2651d7782dca4528a1940
Your Name <you@example.com>
2025-06-16 15:36:35 +0300

    Additionally: Checking access to the blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'status', 'created_at')
+    list_filter = ('status',)
+    search_fields = ('title', 'content')
+
+class PostAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return request.user.has_perm('blog.add_post')
