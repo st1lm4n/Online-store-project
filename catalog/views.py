602043c95c436e2d37cad6a4b8b89c3cd196c789
Your Name <you@example.com>
2025-06-20 12:54:19 +0300

    Low-level caching of the product list Modify the service function services.py

--- catalog/views.py
+++ catalog/views.py
@@ -15,8 +15,8 @@ from django.views.generic import (
 
 from blog.models import Post
 from .forms import ProductForm
-from .models import Product
-from ..services import get_products_by_category
+from .models import Product, Category
+from ..services import get_products_by_category, get_cached_products
 
 
 @method_decorator(cache_page(60 * 15), name='dispatch')  # Кеш на 15 минут
@@ -38,6 +38,14 @@ class CategoryProductsView(ListView):
         context = super().get_context_data(**kwargs)
         context['category'] = Category.objects.get(slug=self.kwargs['category_slug'])
         return context
+
+
+class ProductListView(ListView):
+    template_name = 'products/product_list.html'
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        return get_cached_products()
 
 
 class HomeView(ListView):
--- services.py
+++ services.py
@@ -1,7 +1,18 @@
 from django.core.cache import cache
+from catalog.models import Product, Category
 from django.conf import settings
 
-from catalog.models import Product
+def get_products_by_category(category_slug):
+    if settings.CACHE_ENABLED:
+        key = f'products_category_{category_slug}'
+        products = cache.get(key)
+        if products is None:
+            products = Product.objects.filter(
+                category__slug=category_slug
+            ).select_related('category')
+            cache.set(key, products, 60 * 15)  # Кеш 15 минут
+        return products
+    return Product.objects.filter(category__slug=category_slug)
 
 
 def get_cached_products():
