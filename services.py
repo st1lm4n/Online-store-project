2b384ac95b9458241a253faedebb0821f9663445
Your Name <you@example.com>
2025-06-20 12:51:12 +0300

    Low-level caching of the product list Modify the service function services.py

--- services.py
+++ services.py
@@ -1,15 +1,15 @@
 from django.core.cache import cache
-from catalog.models import Product, Category
 from django.conf import settings
 
-def get_products_by_category(category_slug):
+from catalog.models import Product
+
+
+def get_cached_products():
     if settings.CACHE_ENABLED:
-        key = f'products_category_{category_slug}'
+        key = 'all_products'
         products = cache.get(key)
         if products is None:
-            products = Product.objects.filter(
-                category__slug=category_slug
-            ).select_related('category')
+            products = Product.objects.all().select_related('category')
             cache.set(key, products, 60 * 15)  # Кеш 15 минут
         return products
-    return Product.objects.filter(category__slug=category_slug)
+    return Product.objects.all()
